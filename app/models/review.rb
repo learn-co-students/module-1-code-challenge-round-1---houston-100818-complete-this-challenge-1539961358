3a2aab7b0d9ca0f173d3c74e8693c1a1c187992a
github username <github email address>
2018-10-19 11:34:07 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,49 @@
 class Review
+
+    @@all = []
+
+    attr_accessor :customer, :restaurant
+
+    def initialize(customer, restaurant)
+        @customer = customer
+        @restaurant = restaurant
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer
+        Customer.all.select do |customer|
+            customer.review == self
+        end
+    end
+
+    def restaurant
+        Restaurant.all.select do |restaurant|
+            restaurant.review == self
+        end
+    end
+
+    def self.rating
+        highest_rating = 0
+        best_rating = nil
+
+        self.all.each do|review|
+            if review.rating > highest_rating
+                highest_rating = review.rating
+            end
+        end
+        rating
+    end
+
+
+    def content
+        "#{review_content}"
+    end
+
+
   
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,27 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+
+
+customer = Customer.new("Susan", "King")
+customer2 = Customer.new("Mary", "Joseph")
+customer3 = Customer.new("Mary", "Joseph")
+
+restaurant = Restaurant.new('foody')
+restaurant2 = Restaurant.new('foodplace')
+restaurant3 = Restaurant.new('eatup')
+
+review = Review.new(customer2, restaurant)
+review2 = Review.new(customer, restaurant3)
+review3 = Review.new(customer3, restaurant2)
+
+
+#add_review = add_review.new(customer2, restaurant1,"lame", 5)
+
+
+
+
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+0 
