79dbcd34777150fb8e7eb87b3b87211c4fce7c45
barbaradecares <barbaradecares@hotmail.com>
2018-10-19 10:54:34 -0500

    done

--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+mcdonalds = Restaurant.new("McDonalds")
+tacobell = Restaurant.new("TacoBell")
+wendys = Restaurant.new("Wendy's")
+
+barbara = Customer.new("Barbara", "Decares")
+bob1 = Customer.new("Bob", "one")
+bob2 = Customer.new("Bob", "two")
+
+review1 = Review.new(barbara, mcdonalds, 0, "blablabla :(")
+review2 = Review.new(barbara, tacobell, 5, "blablabla <3")
+review3 = Review.new(barbara, wendys, 2, "blablabla")
+
+review4 = Review.new(bob1, mcdonalds, 4, "bladhuashdias")
+review5 = Review.new(bob2, wendys, 0, "aaaaaasgduisguidasudhuaisduiasduiashduihasiudhuisahduisahduihasdhuasiua")
+bob1.add_review(tacobell, "lalalalal", 5)
+bob1.add_review(tacobell, "lolololo", 4)
+barbara.num_reviews
+mcdonalds.customers
+mcdonalds.average_star_rating
+wendys.longest_review
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
