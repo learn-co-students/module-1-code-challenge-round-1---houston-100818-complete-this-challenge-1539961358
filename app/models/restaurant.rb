526873cc4775a6bd2154f3429c2c2d3b10580e33
Robert Schulte <schulte.robertw@outlook.com>
2018-10-19 11:16:12 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,59 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
-    @name = name
+    self.name = name
+    @@all << self
+  end
+
+  def self.all
+    # returns an array of all restaurants
+    @@all
+  end
+
+  def self.find_by_name(name)
+    # given a string of restaurant name, returns the first restaurant that matches
+    self.all.find do | restaurant |
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    # returns an array of all reviews for that restaurant
+    Review.all.select do | review |
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    # Returns a **unique** list of all customers who have reviewed a particular restaurant.
+    customers = self.reviews.map do | review |
+      review.customer
+    end
+    customers.uniq
+  end
+
+  def average_star_rating
+    # returns the average star rating for a restaurant based on its reviews
+    sum = 0.0
+    self.reviews.each do | review |
+      sum += review.rating
+    end
+    sum / self.reviews.length
+  end
+
+  def longest_review
+    # returns the longest review content for a given restaurant
+    longest_count = 0
+    longest_review = nil
+    self.reviews.each do | review |
+      if review.content.length > longest_count
+        longest_count = review.content.length
+        longest_review = review
+      end
+    end
+    longest_review.content
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+altuve = Customer.new("Jose", "Altuve")
+marwin = Customer.new("Marwin", "Gonzalez")
+verlander = Customer.new("Justin", "Verlander")
+lima = Customer.new("Jose", "Lima")
+
+coppa = Restaurant.new("Coppa Osteria")
+brennans = Restaurant.new("Brennans")
+mala = Restaurant.new("Mala Sichuan Bistro")
+blt = Restaurant.new("Better Luck Tomorrow")
+
+altuve_coppa1 = altuve.add_review(restaurant: coppa, rating: 4, content: "Excellent wine list!")
+marwin_brennans = marwin.add_review(restaurant: brennans, rating: 5, content: "¡Voy a regresar pronto por la sopa de torguga!")
+verlander_mala = verlander.add_review(restaurant: mala, rating: 4, content: "Best Sichuan food in town!")
+verlander_coppa1 = verlander.add_review(restaurant: coppa, rating: 3, content: "Great food, but subpar service.")
+verlander_coppa2 = verlander.add_review(restaurant: coppa, rating: 5, content: "Service was much improved this time!")
+altuve_coppa2 = altuve.add_review(restaurant: coppa, rating: 5, content: "Get the bolognese!")
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
