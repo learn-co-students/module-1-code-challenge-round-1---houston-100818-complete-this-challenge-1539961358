0109a2c673d9c223612a49dddb50b41cfaaa2597
ch0mper <hannahchang17@gmail.com>
2018-10-19 10:40:17 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,59 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(full_name)
+    @@all.find do |customer|
+      customer.full_name == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    array = Review.all.select do |review|
+      review.customer == self
+    end
+    array.length
+  end
+
+  def restaurants
+    array = Review.all.select do |review|
+      review.customer == self
+    end
+    another_array = array.map do |review|
+      review.restaurant
+    end
+    another_array.uniq
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,57 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def customers
+    array = Review.all.select do |review|
+      review.restaurant == self
+    end
+    another_one = array.map do |review|
+      review.customer
+    end
+    another_one.uniq
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    array = self.reviews.map do |review|
+      review.rating
+    end
+    array.inject(0.0) { |sum, el| sum + el } / array.size
+  end
+
+  def longest_review
+    the_longest_count = 0
+    the_review = nil
+
+    self.reviews.map do |review|
+      if review.content.length > the_longest_count
+        the_longest_count = review.content.length
+        the_review = review
+      end
+    end
+    the_review.content
   end
 
 end
