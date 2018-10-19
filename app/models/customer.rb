59aed0920598d4e6e94f4e48468191770202cdd0
Megan Farley <megfarley4@gmail.com>
2018-10-19 11:21:26 -0500

    meg done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,58 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def restaurants
+    cust_reviews = Review.all.select do |review|
+      review.customer == self
+    end
+    cust_reviews.collect do |review|
+      review.restaurant
+    end
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |names|
+      "#{names.first_name} #{names.last_name}" == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select do |names|
+      "#{names.first_name}" == name
+    end
+  end
+
+  def self.all_names
+    self.all.collect do |names|
+      "#{names.first_name} #{names.last_name}"
+    end
+  end
+
+  def num_reviews
+    cust_reviews = Review.all.select do |review|
+      review.customer == self
+    end
+    cust_reviews.length
+  end
+
 end
