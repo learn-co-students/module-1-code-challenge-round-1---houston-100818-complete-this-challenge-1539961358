01f6aad95eedc65d2433c882f54d1a193ae40847
AHCarl <ahcarlin@gmail.com>
2018-10-19 11:15:51 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    r = Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map {|c| c.customer}
+  end
+
+  def average_star_rating
+    a= self.reviews.map {|review| review.rating}
+    sum = 0
+    a.each { |i| sum += i}
+    sum / a.count
+  end
+
+  def longest_review
+    b= self.reviews.map {|rev| rev.content}
+    b.max_by(&:length)
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|i| i.name == name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,15 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+tom = Customer.new("tom", "cullen")
+dick = Customer.new("dick", "whitman")
+dennys = Restaurant.new("dennys")
+ihop = Restaurant.new("IHOP")
+tom_dennys = Review.new(tom, dennys, 5)
+dick_dennys = Review.new(dick, dennys, 5, "GRAND SLAM!")
+dick_ihop = Review.new(dick, ihop, 3, "The Tutti Fruity was not All-Rudy")
+tom.add_review(ihop, 5)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
